11f3fa2d3db03c8286187614c2e6f3d0a44132f5
Ankur Goyal <ankrgyl@gmail.com>
2025-08-31 07:58:56 -0700

    bedrock

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,6 +2,14 @@
 Provider-specific API type definitions.
 */
 
+#[cfg(feature = "anthropic")]
 pub mod anthropic;
+
+#[cfg(feature = "bedrock")]
+pub mod bedrock;
+
+#[cfg(feature = "google")]
 pub mod google;
+
+#[cfg(feature = "openai")]
 pub mod openai;
