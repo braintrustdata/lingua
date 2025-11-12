e14c476fd98a581e5015090fa7dbfae5d338d150
Ken Jiang <ken@braintrustdata.com>
2025-11-12 17:32:42 -0500

    rename to big_serde_json

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-// Re-export lingua_serde_json as serde_json so all code can use serde_json:: after importing
+// Re-export big_serde_json as serde_json so all code can use serde_json:: after importing
 // `use crate::serde_json;`. This wrapper isolates the arbitrary_precision feature.
-pub use lingua_serde_json as serde_json;
+pub use big_serde_json as serde_json;
 
 pub mod capabilities;
 pub mod processing;
