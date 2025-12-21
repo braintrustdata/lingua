e0f6ba927808230d1e2ad1eadd1fa9eceb9dcae8
Ankur Goyal <ankrgyl@gmail.com>
2025-12-21 11:35:19 -0800

    Use wasm-pack (#36)
    Rework packaging to split the wasm export and the typescript wrapper, so you have more control over how the library is loaded.

--- bindings/typescript/vitest.config.ts
+++ bindings/typescript/vitest.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import path from 'path';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+  plugins: [wasm(), topLevelAwait()],
   test: {
     globals: true,
     environment: 'node',
