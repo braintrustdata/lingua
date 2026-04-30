f4cb1921f1886a8769261266d448a83745ffdd8d
Ankur Goyal <ankrgyl@gmail.com>
2026-04-30 15:49:59 -0400

    consolidate type exports

--- bindings/typescript/src/index.ts
+++ bindings/typescript/src/index.ts
@@ -13,48 +13,7 @@ setWasm(wasm);
 export { ensureOnce, getWasm };
 
 export * from "./wasm";
-
-// Re-export all generated types
-export * from "./generated/Message";
-export * from "./generated/AssistantContent";
-export * from "./generated/AssistantContentPart";
-export * from "./generated/GeneratedFileContentPart";
-export * from "./generated/ProviderMetadata";
-export * from "./generated/ProviderOptions";
-export * from "./generated/SourceContentPart";
-export * from "./generated/SourceType";
-export * from "./generated/TextContentPart";
-export * from "./generated/ToolCallArguments";
-export * from "./generated/ToolCallContentPart";
-export * from "./generated/ToolContentPart";
-export * from "./generated/ToolErrorContentPart";
-export * from "./generated/ToolResultContentPart";
-export * from "./generated/ToolResultResponsePart";
-export * from "./generated/UserContent";
-export * from "./generated/UserContentPart";
-
-// Universal request types
-export * from "./generated/UniversalRequest";
-export * from "./generated/UniversalParams";
-export * from "./generated/ProviderFormat";
-
-// Configuration types
-export * from "./generated/ReasoningConfig";
-export * from "./generated/ReasoningEffort";
-export * from "./generated/ReasoningCanonical";
-export * from "./generated/SummaryMode";
-export * from "./generated/ToolChoiceConfig";
-export * from "./generated/ToolChoiceMode";
-export * from "./generated/ResponseFormatConfig";
-export * from "./generated/ResponseFormatType";
-export * from "./generated/JsonSchemaConfig";
-
-// Tool types
-export * from "./generated/UniversalTool";
-export * from "./generated/UniversalToolType";
-
-// Main type aliases for convenience
-export type { Message } from "./generated/Message";
+export type * from "./types";
 
 // Version info
 export { VERSION } from "./version";
---
+++ bindings/typescript/src/types.ts
@@ -0,0 +1,40 @@
+export type { Message } from "./generated/Message";
+export type { AssistantContent } from "./generated/AssistantContent";
+export type { AssistantContentPart } from "./generated/AssistantContentPart";
+export type { GeneratedFileContentPart } from "./generated/GeneratedFileContentPart";
+export type { ProviderMetadata } from "./generated/ProviderMetadata";
+export type { ProviderOptions } from "./generated/ProviderOptions";
+export type { SourceContentPart } from "./generated/SourceContentPart";
+export type { SourceType } from "./generated/SourceType";
+export type { TextContentPart } from "./generated/TextContentPart";
+export type { ToolCallArguments } from "./generated/ToolCallArguments";
+export type { ToolCallContentPart } from "./generated/ToolCallContentPart";
+export type { ToolContentPart } from "./generated/ToolContentPart";
+export type { ToolErrorContentPart } from "./generated/ToolErrorContentPart";
+export type { ToolResultContentPart } from "./generated/ToolResultContentPart";
+export type { ToolResultResponsePart } from "./generated/ToolResultResponsePart";
+export type { UserContent } from "./generated/UserContent";
+export type { UserContentPart } from "./generated/UserContentPart";
+
+export type { UniversalRequest } from "./generated/UniversalRequest";
+export type { UniversalParams } from "./generated/UniversalParams";
+export type { ProviderFormat } from "./generated/ProviderFormat";
+
+export type { ReasoningConfig } from "./generated/ReasoningConfig";
+export type { ReasoningEffort } from "./generated/ReasoningEffort";
+export type { ReasoningCanonical } from "./generated/ReasoningCanonical";
+export type { SummaryMode } from "./generated/SummaryMode";
+export type { ToolChoiceConfig } from "./generated/ToolChoiceConfig";
+export type { ToolChoiceMode } from "./generated/ToolChoiceMode";
+export type { ResponseFormatConfig } from "./generated/ResponseFormatConfig";
+export type { ResponseFormatType } from "./generated/ResponseFormatType";
+export type { JsonSchemaConfig } from "./generated/JsonSchemaConfig";
+
+export type { UniversalTool } from "./generated/UniversalTool";
+export type { UniversalToolType } from "./generated/UniversalToolType";
+export type { BuiltinToolProvider } from "./generated/BuiltinToolProvider";
+export type { TokenBudget } from "./generated/TokenBudget";
+
+export type { ChatCompletionRequestMessage } from "./generated/openai/ChatCompletionRequestMessage";
+export type { InputItem } from "./generated/openai/InputItem";
+export type { InputMessage } from "./generated/anthropic/InputMessage";
